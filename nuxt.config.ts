d7c4cde0058717285f945d7f4330300d593542a1
Lars Schuitema <larsschuitema@funda.nl>
2021-02-14 21:53:40 +0100

    feat(meta): add default meta head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 import { NuxtConfig } from '@nuxt/types'
 
 const config: NuxtConfig = {
+  head: {
+    title: 'Search houses and appartments for sale in the Netherlands [funda]',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Find your new home on funda! Search homes for sale in the Netherlands.'
+      }
+    ],
+  },
   buildModules: ['@nuxt/typescript-build', '@nuxtjs/tailwindcss'],
   components: true
 }
